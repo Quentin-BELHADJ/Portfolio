a2de0d3949983c3fdc00848f68930564ea5b6f57
Quentin Belhadj <quentin.belhadj@edu.univ-fcomte.fr>
2025-06-11 10:25:45 +0200

    navbar dynamique

--- navbar.js
+++ navbar.js
@@ -5,4 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
 			document.getElementById("navbar-container").innerHTML = data;
 		})
 		.catch(error => console.error("Erreur lors du chargement de la navbar :", error));
+
+        let lastScrollTop = 0;
+        let navbar;
+        
+        document.addEventListener("scroll", function () {
+            if (!navbar) {
+                navbar = document.querySelector(".navbar");
+                if (!navbar) return;
+            }
+        
+            let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        
+            if (currentScroll > lastScrollTop) {
+                // Scroll vers le bas : cacher la navbar
+                navbar.style.top = "-100px";
+            } else {
+                // Scroll vers le haut : afficher la navbar
+                navbar.style.top = "0";
+            }
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+        });        
+
 });
